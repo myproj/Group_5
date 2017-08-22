dea5121bf13d1c6cbdcfe6f36af16429626570b4
pankajsh123 <pankaj_s@students.iitmandi.ac.in>
2017-08-22 17:15:45 +0530

    factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,11 @@
 #include "functions.h"
-
+#include<iostream>
+using namespace std;
 int factorial(int n){
+    if(n<0)
+    	{ cout<<"invalid enter another no"<< endl;
+    		cin>>n;
+    	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
