4be35d558f897eb2f347d2c90f0fdc2c2b64b834
gurmeet_singh <gurmeet_singh@students.iitmandi.ac.in>
2017-08-29 04:06:09 +0530

    dcv

--- factorial.cpp
+++ factorial.cpp
@@ -2,10 +2,6 @@
 #include<iostream>
 using namespace std;
 int factorial(int n){
-    if(n<0)
-    	{ cout<<"invalid number enter another no"<< endl;
-    		cin>>n;
-    	}
     	
     if(n!=1){
 	return(n * factorial(n-1));
